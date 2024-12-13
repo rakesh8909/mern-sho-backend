ec75a758faf7ac2ac06bccb43973d1953c2c9286
Bonagiri Bala Rakesh <balarakesh8909@gmail.com>
2024-12-13 12:24:45 +0530

    Update paymentController.js

--- controllers/payment/paymentController.js
+++ controllers/payment/paymentController.js
@@ -7,7 +7,7 @@ const withdrowRequest = require('../../models/withdrowRequest')
 const {v4: uuidv4} = require('uuid')
 const { responseReturn } = require('../../utiles/response')
 const { mongo: {ObjectId}} = require('mongoose')
-const stripe = require('stripe')('sk_test_51Oml5cGAwoXiNtjJZbPFBKav0pyrR8GSwzUaLHLhInsyeCa4HI8kKf2IcNeUXc8jc8XVzBJyqjKnDLX9MlRjohrL003UDGPZgQ')
+const stripe = require('stripe')('')
 
 
 class paymentController{
